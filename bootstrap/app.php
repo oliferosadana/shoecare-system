b5f49d4bcc8ce806097f89f6a1309ed8bd822263
oliferosadana <oliferosadana@gmail.com>
2026-05-15 18:40:21 +0800

    Fix proxy prefix causing malformed login URL

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -20,8 +20,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->trustProxies(at: '*', headers: Request::HEADER_X_FORWARDED_FOR
             | Request::HEADER_X_FORWARDED_HOST
             | Request::HEADER_X_FORWARDED_PORT
-            | Request::HEADER_X_FORWARDED_PROTO
-            | Request::HEADER_X_FORWARDED_PREFIX);
+            | Request::HEADER_X_FORWARDED_PROTO);
         $middleware->web(append: [
             SecurityHeaders::class,
         ]);
